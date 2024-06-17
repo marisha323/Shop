edd2eba329900de6338af7a93f0a0149627716a6
Marina <marisham1223@gmail.com>
2024-06-17 23:03:26 +0300

    welcome 4444

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
