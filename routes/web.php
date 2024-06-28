af59c367d18828b3dc939bd0731656935e1b80cb
Marina <marisham1223@gmail.com>
2024-06-28 12:25:07 +0300

    webphp

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
