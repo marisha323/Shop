2c8a7dd8a15f64ef3bf730d03012fb62cd3cbb1f
Iryna1122 <Oliinykirina1122@gmail.com>
2024-07-14 17:08:28 +0300

    crud Color, brand.

--- resources/views/Brand/index.blade.php
+++ resources/views/Brand/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Список категорий</title>
+    <title>Список брендов</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
